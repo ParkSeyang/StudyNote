03cd2dadd6a27b811a6dbb89a08d3230699281f4
ParkSeyang <ghlwkd14@naver.com>
2025-02-28 21:57:49 +0900

    ㄱㄷㅇ

--- C++ Study Note/C++ Study Note.cpp
+++ C++ Study Note/C++ Study Note.cpp
@@ -11,19 +11,31 @@ private:
 public:
 	MyString(char c)
 	{
-
+		
+		stringpointer = new char[1];
+		stringpointer[0] = c;
+		size = 1;
 	}
 	MyString(const char* str)
 	{
-
+		size = strlen(str);
+		stringpointer = new char[size];
+		for (int i = 0; i < size; i++)
+		{
+			stringpointer[i] = str[i];
+		}
 	}
 	MyString(const MyString& str)
 	{
-
+		size = str.size;
+		for (int i = 0; i != size; i++)
+		{
+			stringpointer[i] = str.stringpointer[i];
+		}
 	}
 	~MyString()
 	{
-
+		delete[] stringpointer;
 	}
 };
 
