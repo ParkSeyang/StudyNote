0863bbc9d6f08da77f8cf0fec144aba8a2b8587e
ParkSeyang <ghlwkd14@naver.com>
2025-02-28 17:54:38 +0900

    123

---
+++ C++ Study Note/C++ Study Note.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+
+using namespace std;
+
+class MyString
+{
+private:
+	char* stringpointer;
+	int size;
+
+public:
+	MyString(char c)
+	{
+
+	}
+	MyString(const char* str)
+	{
+
+	}
+	MyString(const MyString& str)
+	{
+
+	}
+	~MyString()
+	{
+
+	}
+};
+
+
+int main()
+{
+
+	return 0;
+}
