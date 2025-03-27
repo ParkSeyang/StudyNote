d9b99eaa4d16e51d408b1c0604cbeee65a7cb18a
ParkSeyang <ghlwkd14@naver.com>
2025-03-27 21:53:29 +0900

    (2025.03.27) 포인터복습

--- StudyNote/StudyNote.c
+++ StudyNote/StudyNote.c
@@ -381,19 +381,19 @@
 // }
 
 // 포인터의 선언과 사용
-int main()
-{
-    int a;
-    int* pa;
-
-    pa = &a;
-    *pa = 10;
-
-    printf("포인터로 a값 출력 : %d \n",*pa);
-    printf("변수명으로 a값 출력 : %d\n", a);
-
-    return 0;
-}
+// int main()
+// {
+//     int a;
+//     int* pa;
+// 
+//     pa = &a;
+//     *pa = 10;
+// 
+//     printf("포인터로 a값 출력 : %d \n",*pa);
+//     printf("변수명으로 a값 출력 : %d\n", a);
+// 
+//     return 0;
+// }
 
 // 포인터의 시작
 // int main(void)
