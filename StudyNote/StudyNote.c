4243f703e70a8deae786a6200bdd986336429e42
ParkSeyang <ghlwkd14@naver.com>
2025-04-01 21:46:58 +0900

    (2025.04.01) 포인터 복습

--- StudyNote/StudyNote.c
+++ StudyNote/StudyNote.c
@@ -459,6 +459,30 @@
 // 	return 0;
 // 	
 // }
+ 
+// 포인터를 사용한 두정수의 합과 평균 계산
+// int main()
+// {
+//     int a = 10, b = 15, total; // 변수의 선언과 초기화
+//     double avg;                // 평균을 저장할 변수
+//     int* pa, * pb;             // 포인터 동시 선언
+//     int* pt = &total;          // 포인터 선언과 초기화
+//     double* pg = &avg;         // double형 포인터 선언과 초기화
+// 
+//     pa = &a;                  // 포인터 pa 에 변수 a 의 주소 대입
+//     pb = &b;                  // 포인터 pb 에 변수 b 의 주소 대입
+// 
+//     *pt = *pa + *pb;          // a값과 b값을 더해 total에 저장
+//     *pg = *pt / 2.0;          // total 값을 2로 나눈 값을 avg에 저장
+// 
+//     printf("두 정수의 값 : %d, %d\n",*pa,*pb); // a 값과 b 값 출력
+//     // 결과 = 두 정수의 값 : 10, 15
+//     printf("두 정수의 합 : %d\n", *pt); // total 값 출력
+//     // 결과 = 두 정수의 합 : 25
+//     printf("두 정수의 평균 : %.1lf\n",*pg);    // avg 값 출력
+//     // 결과 = 두 정수의 평균 : 12.5
+//     return 0;
+// }
 #pragma endregion
 
 
