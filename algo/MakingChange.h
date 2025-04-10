ad0a0eb2c4aea957562a92b4096388a265c48f72
ParkSeyang <ghlwkd14@naver.com>
2025-04-11 06:39:59 +0900

    123

--- algo/MakingChange.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-void GetChange(int Price, int Pay, int CoinUnits[], int Change[], int Size);
-
-int CountCoins(int Amount, int CoinUnit);
-
-void PrintChange(int CoinUnits[], int Change[], int Size);
