50732756754b95072234ff765bf52891a190ac5a
ParkSeyang <ghlwkd14@naver.com>
2025-04-24 15:43:00 +0900

    123

--- Programing quiz Note/Programing quiz Note.cpp
+++ Programing quiz Note/Programing quiz Note.cpp
@@ -114,11 +114,60 @@ using namespace std;
 #pragma endregion
 
 #pragma region 덧셈식 출력하기
-int main(void) {
-    int a;
-    int b;
-    cin >> a >> b;
-    cout << a + b << endl;
-    return 0;
+// int main(void) {
+//     int a;
+//     int b;
+//     cin >> a >> b;
+//     cout << a + b << endl;
+//     return 0;
+// }
+#pragma endregion
+
+#pragma region 주사위 게임 2
+/*
+* 문제 설명
+* 1부터 6까지 숫자가 적힌 주사위가 세 개 있습니다. 세 주사위를 굴렸을 때 나온 숫자를 각각 a, b, c라고 했을 때 얻는 점수는 다음과 같습니다.
+*
+* 세 숫자가 모두 다르다면 a + b + c 점을 얻습니다.
+* 세 숫자 중 어느 두 숫자는 같고 나머지 다른 숫자는 다르다면 (a + b + c) × (a2 + b2 + c2 )점을 얻습니다.
+* 세 숫자가 모두 같다면 (a + b + c) × (a2 + b2 + c2 ) × (a3 + b3 + c3 )점을 얻습니다.
+* 세 정수 a, b, c가 매개변수로 주어질 때, 얻는 점수를 return 하는 solution 함수를 작성해 주세요.
+*
+* 제한사항
+* a, b, c는 1이상 6이하의 정수입니다.
+*/
+#include <vector>
+
+using namespace std;
+
+int solution(int a, int b, int c) 
+{
+
+    int answer = 0;
+
+	if (a != b != c)
+	{
+		answer = a + b + c;
+	
+	}
+	if (a != b == c || a == b != c)
+	{
+		answer = (a + b + c) * ((a*a) + (b*b) + (c*c));
+		
+	}
+	if (a == b == c)
+	{
+		answer = (a + b + c) * ((a * a) + (b * b) + (c * c)) *((a*a*a)+(b*b*b)+(c*c*c));
+		
+	}
+		return answer;
+}
+int main()
+{
+	int result = 0;
+
+	result = solution(5, 4, 1);
+
+	std::cout << "주사위 값 : " << result <<std::endl;
 }
 #pragma endregion
