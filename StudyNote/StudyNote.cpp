0283e45704db873788777b5465224657154b59f9
ParkSeyang <ghlwkd14@naver.com>
2025-06-27 21:33:12 +0900

    주사위 게임 2풀기

--- StudyNote/StudyNote.cpp
+++ StudyNote/StudyNote.cpp
@@ -1,10 +1,162 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <cmath>
 
 using namespace std;
 
-#pragma region MyRegion
+#pragma region 나머지 구하기 (정답률91%)
+// int solution(int num1, int num2)
+// {
+//     int answer;
+//     if (num1 % num2 <= 1)
+//     {
+//         return answer = num1 % num2;
+//     }
+//     else
+//     {
+//         return answer = num1 % num2;
+//     }
+// 
+// }
+// 
+// int main()
+// {
+//     int answer = solution(10, 5);
+// 
+//     cout << answer << endl;
+// }
+
+#pragma endregion
+
+#pragma region n의 배수 (정답률 90%)
+/* 문제 설명
+* 정수 num과 n이 매개 변수로 주어질 때, num이 n의 배수이면 1을 return n의 배수가 아니라면 0을 return하도록 solution 함수를 완성해주세요.
+*
+* 제한사항
+* 2 ≤ num ≤ 100
+* 2 ≤ n ≤ 9
+* 
+* 입출력 예
+* num	n	result
+* 98	2	1
+* 34	3	0
+* 
+* 입출력 예 설명
+* 입출력 예 #1
+* 98은 2의 배수이므로 1을 return합니다.
+* 입출력 예 #2
+* 32는 3의 배수가 아니므로 0을 return합니다.
+* 
+* 
+*/
+// int solution(int num, int n)  // 내가 쓴 풀이
+// {
+//     int answer = 0;
+// 
+//     if (num % n == 0)
+//     {
+//         return answer += 1;
+//     }
+//     else if (num % n != 0)
+//     {
+//         return answer;
+//     }
+// 
+// }
+// 
+//int solution(int num, int n) // 다른사람이푼 풀이 위의 내가작성한 코드와 같은 코드이다. 좀더 간결하게 작성되어있다.
+//{
+//    return num % n == 0 ? 1 : 0;
+//}
+//
+//int main()
+//{
+//    int answer = solution(98, 3);
+//    cout << answer << endl;
+//}
+
+
+#pragma endregion
+
+#pragma region 주사위 게임 2 (정답률 89%)
+/* 문제 설명
+* 1부터 6까지 숫자가 적힌 주사위가 세 개 있습니다. 세 주사위를 굴렸을 때 나온 숫자를 각각 a, b, c라고 했을 때 얻는 점수는 다음과 같습니다.
+* 
+* 세 숫자가 모두 다르다면 a + b + c 점을 얻습니다.
+* 세 숫자 중 어느 두 숫자는 같고 나머지 다른 숫자는 다르다면 (a + b + c) × (a2 + b2 + c2 )점을 얻습니다.
+* 세 숫자가 모두 같다면 (a + b + c) × (a2 + b2 + c2 ) × (a3 + b3 + c3 )점을 얻습니다.
+* 세 정수 a, b, c가 매개변수로 주어질 때, 얻는 점수를 return 하는 solution 함수를 작성해 주세요.
+* 
+* 제한사항
+* a, b, c는 1이상 6이하의 정수입니다.
+* 입출력 예
+* a	b	c	result
+* 2	6	1	9
+* 5	3	3	473
+* 4	4	4	110592
+* 입출력 예 설명
+* 입출력 예 #1
+* 
+* 예제 1번에서 세 주사위 숫자가 모두 다르므로 2 + 6 + 1 = 9점을 얻습니다. 따라서 9를 return 합니다.
+* 입출력 예 #2
+* 
+* 예제 2번에서 두 주사위 숫자만 같으므로 (5 + 3 + 3) × (52 + 32 + 32 ) = 11 × 43 = 473점을 얻습니다. 따라서 473을 return 합니다.
+* 입출력 예 #3
+* 
+* 예제 3번에서 세 주사위 숫자가 모두 같으므로 (4 + 4 + 4) × (42 + 42 + 42 ) × (43 + 43 + 43 ) = 12 × 48 × 192 = 110,592점을 얻습니다. 따라서 110592를 return 합니다.
+*/
+
+// int solution(int a, int b, int c) {
+//     int answer = 0;
+//    
+//     if (a != b && b != c && a != c)
+//     {
+//         return answer = a + b + c;
+//     }
+//     else if(a == b || b == c || a == c)
+//     {
+//         return answer = (a + b + c) * static_cast<int>(pow(a, 2) + pow(b, 2) + pow(c, 2));
+//     }
+//     else
+//     {
+//         return answer = (a + b + c) * static_cast<int>(pow(a, 2) + pow(b, 2) + pow(c, 2)) * static_cast<int>(pow(a, 3) + pow(b, 3) + pow(c, 3));
+//     }
+//     
+// }
+// int solution(int a, int b, int c) 
+// {
+//     int answer1 = a + b + c;
+//     int answer2 = a * a + b * b + c * c;
+//     int answer3 = a * a * a + b * b * b + c * c * c;
+// 
+//     if (a == b && b == c)
+//     {
+//         return answer1 * answer2 * answer3;
+//         
+//     }
+//     else if (a == b || b == c || a == c)
+//     {
+//         return answer1 * answer2;
+//     }
+//     else
+//     {
+//         return answer1;
+//     }
+// 
+// }
+// int main()
+// {
+//     int answer = solution(3, 5, 4);
+// 
+//     cout << answer << endl;
+// }
+
+
+#pragma endregion
+
+#pragma region 음양 더하기 (정답률 87%)
+
 /*
 * absolutes의 길이는 1 이상 1,000 이하입니다.
 * absolutes의 모든 수는 각각 1 이상 1,000 이하입니다.
