3a57bb3af3f0e79cdafd6f87b10d116d825b9652
ParkSeyang <ghlwkd14@naver.com>
2025-10-28 10:17:27 +0900

    123

--- StudyNote2/StudyNote2.cpp
+++ StudyNote2/StudyNote2.cpp
@@ -4,25 +4,32 @@
 
 using namespace std;
 
+// 행렬의 덧셈
 vector<vector<int>> solution(vector<vector<int>> arr1, vector<vector<int>> arr2)
 {
     vector<vector<int>> answer;
     for (int i = 0; i < arr1.size(); i++)
     {
-        for (int j = 0; j < i; j++)
+        for (int j = 0; j < arr1.size(); j++)
         {
-            answer[i][j] = arr1[i][j] + arr2[i][j];
+            answer[j][i] = arr1[j][j + 1] + arr2[j][j + 1];
         }
     }
 
     return answer;
 }
 
-void STQ();
-
 int main()
 {
-    vector<vector<int>> array1;
-    vector<vector<int>> array2;
+    vector<vector<int>> array1(3,vector<int>(3));
+
+    vector<vector<int>> array2(3);
+
+    vector<int> test1(5, 5);
+
+
+    cout << test1[0][1] << endl;
+
+   // cout << solution(array1, array2) << endl;
 
 }
